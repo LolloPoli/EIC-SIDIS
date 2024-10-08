9df057c2ade9c0f14af469e805d6e536069efe63
Lorenzo Polizzi <149718274+LolloPoli@users.noreply.github.com>
2024-10-08 15:03:22 +0200

    Create run_all.C

---
+++ run_all.C
@@ -0,0 +1,44 @@
+void run() {
+
+    main11();
+    main12();
+    main13();
+    main14();
+    main15();
+    main16();
+    main17();
+    std::cout << " " << std::endl;
+    std::cout << " --------------- 0.25 ------------------ " << std::endl;
+    std::cout << " " << std::endl;
+    main18();
+    main19();
+    main20();
+    main21();
+    main22();
+    main23();
+    main24();
+    main25();
+    std::cout << " " << std::endl;
+    std::cout << " --------------- 0.50 ------------------ " << std::endl;
+    std::cout << " " << std::endl;
+    main26();
+    main27();
+    main28();
+    main29();
+    main30();
+    main31();
+    main32();
+    std::cout << " " << std::endl;
+    std::cout << " --------------- 0.75 ------------------ " << std::endl;
+    std::cout << " " << std::endl;
+    main33();
+    main34();
+    main35();
+    main36();
+    main37();
+    main38();
+    main39();
+    main40();
+
+    std::cout << "Tutti i main sono stati calcolati." << std::endl;
+}
